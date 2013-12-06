671fc1f1aa7a677870533db74047c604e541547f
Björn Ax <bjorn.ax@gmail.com>
2013-12-06 13:29:23 +0100

    Update db-cred.inc.php

--- sys/config/db-cred.inc.php
+++ sys/config/db-cred.inc.php
@@ -7,20 +7,20 @@ $C = array();
 /*
  * The database host URL
  */
-$C['DB_HOST'] = 'transwarp-157713.mysql.binero.se';
+$C['DB_HOST'] = '';
 
 /*
  * The database username
  */
-$C['DB_USER'] = '157713_hg11626';
+$C['DB_USER'] = '';
 
 /*
  * The database password
  */
-$C['DB_PASS'] = 'booklamp820319';
+$C['DB_PASS'] = '';
 
 /*
  * The name of the database to work with
  */
-$C['DB_NAME'] = '157713-transwarp';
+$C['DB_NAME'] = '';
 ?>
